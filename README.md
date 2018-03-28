d2d5cefdb2f9de9b13d3a823f47ceaa96fd8e938
AHN HO JUNG <labeldock@me.com>
2018-03-28 18:36:58 +0900

    update

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # vue-simple
 Very simple vue template
+
+## Installation
+This is a project template for [vue-cli](https://github.com/vuejs/vue-cli)(vue-cli 2.x<=).
+
+``` bash
+vue init temping/vue-simple myproject
+```
--- meta.js
+++ meta.js
@@ -1,3 +1,34 @@
-{
-  "schema": {}
+module.exports = {
+  helpers: {
+    raw: function(options) {
+      return options.fn(this)
+    }
+  },
+  prompts: {
+    name: {
+      type: "string",
+      required: true,
+      message: "App name"
+    },
+    description: {
+      type: "string",
+      required: false,
+      message: "App description",
+      default: "Vue simple app"
+    },
+    author: {
+      type: "string",
+      required: false,
+      message: "Author"
+    }
+  },
+  completeMessage:[
+    "{{#inPlace}}",
+    "To get started:\n\n  npm install \n  npm run dev",
+    "\n\nshortcut:\n\n  npm i && npm run start",
+    "{{else}}",
+    "To get started:\n\n  cd {{destDirName}}\n  npm install \n  npm run start",
+    "\n\nshortcut:\n\n  cd {{destDirName}} && npm i && npm run start",
+    "{{/inPlace}}"
+  ].join("")
 }
